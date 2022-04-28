d8aef87f5cdb8433334cebfdd663c32e4c58cc0a
Nazareno Mateo <nazamateo@gmail.com>
2022-04-28 22:59:33 +0800

    done create read update delete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,71 @@
 import { useState } from "react";
+import List from "./Components/List";
 
 //create read update delete
 
 function App() {
-  return <></>;
+  const [userData, setUserData] = useState([]);
+  const [name, setName] = useState();
+  const [surname, setSurname] = useState();
+  const [userId, setUserId] = useState(0);
+
+  const resetter = () => {
+    setName("");
+    setSurname("");
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setUserData([...userData, { name, surname, userId }]);
+    setUserId(userId + 1);
+    resetter();
+  };
+  const handleClearAll = (e) => {
+    e.preventDefault();
+    setUserData([]);
+    setUserId(0);
+  };
+  const deleteUserData = (id) => {
+    const newUserData = userData.filter((data) => {
+      return userData.indexOf(data) !== id;
+    });
+    setUserData(newUserData);
+  };
+
+  const updateUserData = (newUserData) => {
+    setUserData(newUserData);
+  };
+
+  return (
+    <>
+      <form className="myForm" onSubmit={handleSubmit}>
+        <input
+          className="myInput"
+          placeholder="name"
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        ></input>
+        <input
+          className="myInput"
+          placeholder="surname"
+          value={surname}
+          onChange={(e) => {
+            setSurname(e.target.value);
+          }}
+        ></input>
+        <button type="submit" className="button">
+          Submit
+        </button>
+      </form>
+      <button onClick={handleClearAll}>Clear Data</button>
+      <List
+        userData={userData}
+        updateUserData={updateUserData}
+        deleteUserData={deleteUserData}
+      />
+    </>
+  );
 }
 
 export default App;
--- src/Components/List.jsx
+++ src/Components/List.jsx
@@ -1,5 +1,100 @@
-import React from "react";
+import React, { useState, useSyncExternalStore } from "react";
+import list from "../Components/List.css";
+export default function List({ userData, updateUserData, deleteUserData }) {
+  const [isUpdating, setIsUpdating] = useState(false);
+  const [updateName, setUpdateName] = useState();
+  const [updateSurname, setUpdateSurname] = useState();
+  const [showUserId, setShowUserId] = useState();
+  const [index, setIndex] = useState();
+  const handleDelete = (e, i) => {
+    e.preventDefault();
+    deleteUserData(i);
+  };
+  const showUpdate = (i) => {
+    setIsUpdating(true);
+    setUpdateName(userData[i].name);
+    setUpdateSurname(userData[i].surname);
+    setShowUserId(userData[i].userId);
+    setIndex(i);
+  };
+  const handleUpdate = (e) => {
+    e.preventDefault();
+    setIsUpdating(false);
+    const newUserData = userData;
+    newUserData[index].name = updateName;
+    newUserData[index].surname = updateSurname;
+    console.log(newUserData);
+    updateUserData(newUserData);
+  };
+  return (
+    <>
+      <div className="data-container">
+        {userData && (
+          <>
+            {isUpdating === true && (
+              <>
+                <p>User Id : {showUserId}</p>
+                <form onSubmit={handleUpdate}>
+                  <input
+                    placeholder="name"
+                    value={updateName}
+                    onChange={(e) => {
+                      setUpdateName(e.target.value);
+                    }}
+                  ></input>
+                  <input
+                    placeholder="surname"
+                    value={updateSurname}
+                    onChange={(e) => {
+                      setUpdateSurname(e.target.value);
+                    }}
+                  ></input>
+                  <button type="submit">Update</button>
+                </form>
+              </>
+            )}
 
-export default List = () => {
-  return <></>;
-};
+            {isUpdating === false && (
+              <table>
+                <tr>
+                  <th>UserId</th>
+                  <th>Name</th>
+                  <th>Surname</th>
+                </tr>
+                {userData.map((data, i) => {
+                  return (
+                    <>
+                      <tr key={i}>
+                        <td>{data.userId}</td>
+                        <td>{data.name}</td>
+                        <td>{data.surname}</td>
+                        <td>
+                          <button
+                            onClick={(e) => {
+                              handleDelete(e, i);
+                            }}
+                          >
+                            Delete
+                          </button>
+                        </td>
+                        <td>
+                          <button
+                            onClick={() => {
+                              showUpdate(i);
+                            }}
+                          >
+                            Update
+                          </button>
+                        </td>
+                      </tr>
+                    </>
+                  );
+                })}
+              </table>
+            )}
+          </>
+        )}
+      </div>
+    </>
+  );
+}
